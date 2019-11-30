2037642a5a3a5fccde74cf81439990b0343c6865
Steven Keller <stkeller@microsoft.com>
2019-11-29 20:59:47 -0700

    Initial Commtt

---
+++ WebAppAgain/WebAppAgain/Models/ConfigHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace WebAppAgain.Models
+{
+    public static class ConfigHelper
+    {
+        public static string GetAppName()
+        {
+            return ConfigurationManager.AppSettings["ApplicationName"] ?? "ERROR";
+
+
+        }
+
+    }
+}
